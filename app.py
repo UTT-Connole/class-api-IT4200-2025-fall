2543c23bd4ff96708576241a58d010eaa379c921
Clint <clint.hardy@utahtech.edu>
2025-08-28 15:02:12 -0600

    added clint-new-branch

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
 	return 'Hello, Flask!'
-
+@app.route('/clint')
+def home():
+	return 'Hello, Clint!'
 if __name__ == '__main__':
 	app.run(debug=True)
