0a3b26ee153a027a26bdc8d9902fdd0311ec7bb8
Ayden Sawyer <aajet604@gmail.com>
2025-08-28 14:13:46 -0600

    Endpoint name update

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
 	return 'Hello, Flask!'
 
+@app.route('/Skylands')
+def home():
+	user_input = input('Enter somthing: ')
+	if user_input == 'Conquretron':
+		return 'K. A. O. S.'
+	else:
+		return 'Wrong Answer'
+
 if __name__ == '__main__':
 	app.run(debug=True)
