7264f33650500bb4d77f846dec5fefa4f7cd08e1
joshua.beckstrand <joshua.beckstrand@utahtech.edu>
2025-08-28 14:27:35 -0600

    my endpoint added

--- app.py
+++ app.py
@@ -6,5 +6,18 @@ app = Flask(__name__)
 def home():
 	return 'Hello, Flask!'
 
+restaurants = [
+    "Chipotle",
+    "Chick-fil-A",
+    "Subway",
+    "Olive Garden",
+    "Five Guys",
+    "Panera Bread"
+]
+@app.route('/randomRestaurant')
+def choose():
+    restaurant = random.choice(restaurants)
+    return jsonify({"restaurant": restaurant})
+
 if __name__ == '__main__':
 	app.run(debug=True)
