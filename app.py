4bc25f53208579c660cdcb836e4b3013a42e4b32
Gillvian <fletcherrty@gmail.com>
2025-08-28 14:29:03 -0600

    app now has input

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/gill')
 def home():
-	return 'my test app'
+    user_input = input('What is your quest?')
+    if user_input == 'We seek the Holy Grail':
+        return "You may pass"
+    else:
+	    return 'You are doomed'
 
 if __name__ == '__main__':
 	app.run(debug=True)
