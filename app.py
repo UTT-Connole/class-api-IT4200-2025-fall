71ab8afb542a9f362e997bb7d9d0d2f4fa1aba6d
Heatherengler31 <heatherengler31@gmail.com>
2025-08-30 03:03:47 -0600

    /space-facts

--- app.py
+++ app.py
@@ -5,6 +5,19 @@ app = Flask(__name__)
 @app.route('/')
 def home():
 	return 'Hello, Flask!'
+@app.route('/heather')
+def home():
+	return 'Hello!'
+@app.route('/space-facts')
+def space_facts():
+    facts = [
+        {"fact": "A day on Venus is longer than a year on Venus."},
+        {"fact": "Neutron stars can spin 600 times per second."},
+        {"fact": "Saturn could float in water because it’s mostly gas."},
+        {"fact": "The footprints on the Moon will likely last millions of years."},
+        {"fact": "Jupiter has 95 known moons as of 2025."}
+    ]
+    return jsonify(random.choice(facts))
 
 if __name__ == '__main__':
 	app.run(debug=True)
