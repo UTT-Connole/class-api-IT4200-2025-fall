c72086c4a1d303ded56bcb662059f20bdcfded9f
joshua.beckstrand <joshua.beckstrand@utahtech.edu>
2025-10-17 17:16:39 -0600

    Major change, changed the endpoint name to make it more clear

--- app.py
+++ app.py
@@ -872,8 +872,8 @@ def page_not_found(e):
     return render_template("404.html"), 404
 
 
-@app.route("/randompkmon")
-def randompkmon():
+@app.route("/random_pokemon")
+def random_pokemon():
     a = random.randint(1, 1010)
     print(f"Redirecting to Pokémon ID: {a}")
     return redirect((f"https://www.pokemon.com/us/pokedex/{a}")), 302
