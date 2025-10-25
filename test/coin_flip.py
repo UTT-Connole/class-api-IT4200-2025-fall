fa6386b9460472a2a047a58d8f2027f5cd2e5966
Heatherengler31 <93070227+Heatherengler31@users.noreply.github.com>
2025-10-25 17:39:43 -0600

    Delete test/coin_flip.py

--- test/coin_flip.py
+++
@@ -1,7 +0,0 @@
-def test_coinflip_json_response(client):
-    res = client.post("/api/coinflip", json={"choice": "heads", "bet": 5})
-    data = res.get_json()
-    assert res.status_code == 200
-    assert data["choice"] in ["heads", "tails"]
-    assert data["result"] in ["heads", "tails"]
-    assert data["outcome"] in ["win", "lose"]
